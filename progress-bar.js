cec3a0e2aac590f4102e0557a96cd6615ea152c4
Jeremy Karlsson <karlsson@jeremy.se>
2018-06-09 00:23:10 +0200

    Add jsdocs

--- progress-bar.js
+++ progress-bar.js
@@ -22,6 +22,11 @@ const ErrorMessages = {
 };
 
 class ProgressBar extends HTMLElement {
+  /**
+   * Renders the progress bar to a shadow DOM, caches references to the wrapper
+   * && indicator elements and adds a click handler to the wrapper element for
+   * handling the seeking.
+   */
   connectedCallback () {
     const sDOM = this.attachShadow({ mode: 'closed' });
 
@@ -38,19 +43,29 @@ class ProgressBar extends HTMLElement {
     this.wrapper.addEventListener('click', event => this.handleClick(event), false);
   }
 
+  /**
+   * Handle clicking the progress bar wrapper. Calculate the percentate
+   * to seek to and emits a progress-bar:seek CustomEvent to be listened
+   * to outside this component to react to the click.
+   *
+   * @param {MouseEvent} event
+   */
   handleClick (event) {
-    const percent = this.getRelativePercMouseEvent(event);
+    const percent = this.getMousePositionAsPercent(event);
+    const detail = { percent };
 
-    document.dispatchEvent(new CustomEvent('progress-bar:seek', {
-      detail: {
-        percent
-      }
-    }));
+    document.dispatchEvent(new CustomEvent('progress-bar:seek', { detail }));
 
     this.animation.currentTime = this.animationDuration * percent;
   }
 
-  getRelativePercMouseEvent (mouse) {
+  /**
+   * Calculates where in the progress bar the person clicked
+   * in percent.
+   *
+   * @param {MouseEvent} mouse
+   */
+  getMousePositionAsPercent (mouse) {
     const boundRect = this.wrapper.getBoundingClientRect();
     const offset = {
       left: Math.abs((boundRect.left + window.pageXOffset) - mouse.pageX),
@@ -60,6 +75,9 @@ class ProgressBar extends HTMLElement {
     return offset.left / offset.width;
   }
 
+  /**
+   * Setting this recrates the animation with the new duraton and pauses the animation.
+   */
   set duration (duration) {
     this.animationDuration = duration * 1000;
 
@@ -78,12 +96,20 @@ class ProgressBar extends HTMLElement {
     this.animation.pause();
   }
 
+  /**
+   * Setting this stops the animations, updates the playback rate and plays it again.
+   */
   set playbackRate (playbackRate) {
     this.stop();
     this.animation.playbackRate = playbackRate;
     this.start();
   }
 
+  /**
+   * Starts the animation if duration is defined.
+   *
+   * @throws Will throw an error if duration is not set.
+   */
   start () {
     if (!this.animation) {
       throw new Error(ErrorMessages.NO_DURATION);
@@ -92,6 +118,11 @@ class ProgressBar extends HTMLElement {
     this.animation.play();
   }
 
+  /**
+   * Stops the animation if duration is defined.
+   *
+   * @throws Will throw an error if duration is not set.
+   */
   stop () {
     if (!this.animation) {
       throw new Error(ErrorMessages.NO_DURATION);
